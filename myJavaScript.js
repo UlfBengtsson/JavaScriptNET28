ca29eed58fde537d6f92357d81c0bac23b3c09ff
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-12-04 11:33:52 +0100

    init

---
+++ myJavaScript.js
@@ -0,0 +1,11 @@
+document.getElementById("welcomeH1").innerHTML = "Hello World Wide Web";
+
+var myDiv = document.getElementById("myDiv");
+
+myDiv.innerHTML = "";
+
+var myBody = document.getElementsByTagName("body")[0];
+
+myBody.style.backgroundColor = "lightgray";
+
+console.log(myDiv);
